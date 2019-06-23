39dc01891d531d1c938e6497802b871f212529c0
DMayanja21 <you@example.com>
2019-06-24 02:18:30 +0300

    edited

--- Contact.js
+++ Contact.js
@@ -1,4 +1,16 @@
-function Contact(first_name,second_name,phone_number,email,address){
+//Alternative
+function Contact(first_name,second_name,phone_number,email,address_array[]){
+	this.first_name=first_name;
+	this.second_name=second_name;
+	this.phone_number=phone_number;
+	this.email=email;
+	this.address=[address_array[0]="street",address_array[1]="city",address_array[2]="country"];
+}
+
+//Objects:
+var danny=new Contact("Daniel","Mutumbo","0778842365","danmuts@gmail.com",["Prince Charles Lane","Kampala","Uganda"]);
+var jack= new Contact("Jack","Black","0702563627","jackblk@yahoo.co.uk",["Union Road","Kampala","Uganda"]);
+/*function Contact(first_name,second_name,phone_number,email,address){
 	this.first_name=first_name;
 	this.second_name=second_name;
 	this.phone_number=phone_number;
@@ -13,7 +25,7 @@ var Address={
 
 //Objects:
 var danny=new Contact("Daniel","Mutumbo","0778842365","danmuts@gmail.com",Address);
-var jack= new Contact("Jack","Black","0702563627","jackblk@yahoo.co.uk",Address);
+var jack= new Contact("Jack","Black","0702563627","jackblk@yahoo.co.uk",Address);*/
 
 Contact.prototype.fullName=function(){
 	return this.first_name+","+this.second_name;
